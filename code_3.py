0e98bbff1ef867039eee9ca58bbd4c65ee2ae938
Filopydred <143886295+filopydred@users.noreply.github.com>
2023-10-20 03:13:50 -0400

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,28 @@
+        }
+
+    public static void main(String[] args) {
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+
+}
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    for (let i = 0; i < 10; i++) {
+}
+    }
+function main() {
+    });
+
+    return data;
+        console.log("Random Number: " + item);
+main();
+function generateRandomData() {
+    data.forEach(item => {
+    const data = [];
+    const data = generateRandomData();
