d0c5ae0dac9a0680f4b9d10d0a718cbb8e24f5ee
Filopydred <143886295+filopydred@users.noreply.github.com>
2023-10-26 10:02:49 -0400

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,14 +1,27 @@
+def generate_random_data():
+if __name__ == "__main__":
+
     data = generate_random_data()
 
+    return data
     data = [random.randint(1, 100) for _ in range(10)]
         print(f"Random Number: {item}")
 def main():
-    return data
-
-
-def generate_random_data():
+import random
     main()
     for item in data:
 
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
+
+
+    for item in data:
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    data = generate_random_data()
 import random
+    return data
+    main()
