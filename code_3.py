608e4d02ac72113acead0c4bcabaebba7d6ae566
Filopydred <143886295+filopydred@users.noreply.github.com>
2023-10-29 13:26:25 -0400

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,28 +1,26 @@
-        }
+    main()
+if __name__ == "__main__":
 
-    public static void main(String[] args) {
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-        }
-public class RandomDataGenerator {
-        for (int i = 0; i < 10; i++) {
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def main():
 
-}
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
-    for (let i = 0; i < 10; i++) {
-}
-    }
-function main() {
-    });
 
-    return data;
-        console.log("Random Number: " + item);
-main();
-function generateRandomData() {
-    data.forEach(item => {
-    const data = [];
-    const data = generateRandomData();
+
+    return data
+import random
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")    data = generate_random_data()
+    return data
+def main():
+
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+import random
+if __name__ == "__main__":
