9a24e5b838a1b81f7070e6833344ca648ee8af49
Filopydred <143886295+filopydred@users.noreply.github.com>
2023-11-01 16:54:20 -0400

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,46 +1,40 @@
-    data = [random.randint(1, 100) for _ in range(10)]
-
-
     for item in data:
     data = generate_random_data()
-    return data
+
     main()
+if __name__ == "__main__":
         print(f"Random Number: {item}")
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def main():
+
+
 
 import random
+if __name__ == "__main__":
+    data = generate_random_data()
+    main()
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+def generate_random_data():
+    return data
+        print(f"Random Number: {item}")
+
+import random    main()
+    return data
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    for item in data:
+if __name__ == "__main__":
+
+    data = generate_random_data()
+
+
+        print(f"Random Number: {item}")
 
 def main():
-if __name__ == "__main__":
-def generate_random_data():function generateRandomData() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
-    }
-    const data = generateRandomData();
-}
-        console.log("Random Number: " + item);
-
-
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-    });
-    return data;
-function main() {
-
-    data.forEach(item => {
-main();    }
-
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
-function generateRandomData() {
-}
-    });
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-}
-function main() {
-
-        console.log("Random Number: " + item);
-main();
-    const data = generateRandomData();
-    return data;
