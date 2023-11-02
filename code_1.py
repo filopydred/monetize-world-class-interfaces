cf8fb9d443a7e83a7303ed619be6dfa887e79cde
Filopydred <143886295+filopydred@users.noreply.github.com>
2023-11-02 18:02:08 -0400

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,27 @@
-        int[] data = new int[10];
-            data[i] = (int) (Math.random() * 100) + 1;
-public class RandomDataGenerator {
+    return data
+if __name__ == "__main__":
+    main()
+    data = generate_random_data()
+def main():
 
-    public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
+import random
 
-        }
-}
-        }
-    }
-        for (int item : data) {
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+    for item in data:
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+def generate_random_data():
+    main()
+def main():
+
+
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+    data = generate_random_data()
